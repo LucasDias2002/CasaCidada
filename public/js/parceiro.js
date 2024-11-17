7723e63007c12372bf7d1cf7047bd12838bcca3c
Lucas Phillipe Freitas Dias <lucas.pfd.2002@gmail.com>
2024-11-17 15:53:44 -0300

    Adicionando a página de excluir parceiro

--- public/js/parceiro.js
+++ public/js/parceiro.js
@@ -1,6 +1,6 @@
-const form = document.getElementById("form-parceiro");
+const form_parceiro = document.getElementById("form-parceiro");
 
-form.addEventListener("submit", async (event) => {
+form_parceiro.addEventListener("submit", async (event) => {
     event.preventDefault();
 
     const nome = document.getElementById("nome").value;
@@ -21,11 +21,11 @@ form.addEventListener("submit", async (event) => {
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({ nome,email, tipo, cnpj, area_atuacao, telefone, cep, numero })
+            body: JSON.stringify({ nome, email, tipo, cnpj, area_atuacao, telefone, cep, numero })
         });
         if (response.ok) {
             alert('Parceiro cadastrado com sucesso!');
-            form.reset();
+            form_parceiro.reset();
         } else {
             alert('Erro ao cadastrar parceiro. Tente novamente.');
         }
@@ -37,6 +37,60 @@ form.addEventListener("submit", async (event) => {
 })
 
 const excluirParceiros = document.getElementById("btn-excluir-parceiro");
-excluirParceiros.addEventListener("click", ()=>{
-    
-});
+excluirParceiros.addEventListener("click", async () => {
+    try {
+        const response = await fetch("/parceiro", {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        if (response.ok) {
+            const parceiros = await response.json();
+            console.log(parceiros.length);
+            let parceiroCard = "";
+            for (let i = 0; i < parceiros.length; i++) {
+                parc = parceiros[i];
+                parceiroCard += `<tr>
+                            <td>${parc.NOME}</td>
+                            <td>${parc.EMAIL}</td>
+                            <td>${parc.TELEFONE}</td>
+                            <td>${parc.AREA_ATUACAO}</td>
+                            <td><button type="button" onclick="ApagarParceiro(${parc.ID})" class="btn btn-danger">Apagar</button></td>
+                </tr>`;
+            }
+            document.getElementById("parceiros").innerHTML = parceiroCard;
+        }
+        else {
+            console.log('Erro ao carregar usuários. Tente novamente.');
+        }
+    }
+    catch (erro) {
+        console.error('Erro na requisição:', erro);
+        alert('Erro ao conectar ao servidor.');
+    }
+}
+);
+
+async function ApagarParceiro(id){
+    try {
+        const response = await fetch(`/parceiro/${id}`, {
+            method: "DELETE",
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        if (response.ok) {
+            alert(`PARCEIRO ${id} foi apagado!`)
+            location.href = "adm.html";
+        }
+        else {
+            console.log('Erro ao apagar parceiro. Tente novamente.');
+        }
+    }
+    catch (erro) {
+        console.error('Erro na requisição:', erro);
+        alert('Erro ao conectar ao servidor.');
+    }
+}
+;
