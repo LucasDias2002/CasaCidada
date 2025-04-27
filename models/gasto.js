54b651835b1a93e3066ef6d817f51b976bb03e7e
Lucas Phillipe Freitas Dias <lucas.pfd.2002@gmail.com>
2025-04-27 13:40:10 -0300

    Adicionando atualizações na model para sequelize

--- models/gasto.js
+++ models/gasto.js
@@ -1,20 +1,6 @@
 const { sequelize } = require("../database/conexaoPostgre");
 const { DataTypes } = require('sequelize');
 
-
-async function ListarUltimo2anos() {
-    try {
-        const result = await sequelize.query(`SELECT TO_CHAR(g.data_gasto, 'MM-YYYY') AS mes, SUM(g.valor) AS total_gastos FROM gastos g WHERE EXTRACT(YEAR FROM g.data_gasto) >= EXTRACT(YEAR FROM CURRENT_DATE) - 2 GROUP BY TO_CHAR(g.data_gasto, 'MM-YYYY') ORDER BY mes;`);
-        return result.rows;
-    } catch (error) {
-        console.error('Erro ao listar gasto por id - Model:', error);
-        throw error;
-    }
-}
-
-
-
-
 const Gasto = sequelize.define('Gasto', {
     id: {
       type: DataTypes.INTEGER,
